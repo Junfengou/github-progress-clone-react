3d83ccba8db440dd1088fd0fa5621ca398f8dca7
Junfeng ou <junfengou@gmail.com>
2020-11-11 14:02:58 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" exact component={Dashboard} />
+				<Route path="/login" component={Login} />
+				<Route path="/*" component={Error} />
+			</Switch>
+		</Router>
+	);
+}
+
+export default App;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Info, Repos, User, Search, Navbar } from "../components";
+import loadingImage from "../images/preloader.gif";
+import { GithubContext } from "../context/context";
+
+function Dashboard() {
+	return (
+		<div>
+			<main>
+				<h2>Dashboard Page</h2>
+			</main>
+		</div>
+	);
+}
+
+export default Dashboard;
